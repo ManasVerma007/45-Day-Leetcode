f25fa6275ef42ac748cb905bf8964813d6aed1ab
Manas <manasverma012345@gmail.com>
2023-09-18 18:14:35 +0530

    Day 9

---
+++ Day 9/141. Linked List Cycle.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None or head.next is None:
+            return False
+
+        slow_ptr = head
+        fast_ptr = head
+
+        while fast_ptr is not None and fast_ptr.next is not None:
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+
+            if slow_ptr == fast_ptr:
+                return True
+
+        return False
+
+            
